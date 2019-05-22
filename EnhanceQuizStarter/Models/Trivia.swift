fd71eb356806961874432dc1ed81e230005de8f2
Dan Morse <morsedan@outlook.com>
2019-05-22 11:19:39 -0700

    Initial Commit

---
+++ EnhanceQuizStarter/Models/Trivia.swift
@@ -0,0 +1,14 @@
+//
+//  Trivia.swift
+//  EnhanceQuizStarter
+//
+//  Created by morse on 5/21/19.
+//  Copyright © 2019 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct Trivia {
+    let question: String
+    let answer: Bool
+}
